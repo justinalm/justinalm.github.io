19131e66f7f39cdddbcad4ca88f88cc64d1ad085
Justin Alm <almjustin@gmail.com>
2018-09-01 18:08:24 -0700

    Improved nav.

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,12 +1,5 @@
-// Basic FitVids Test
 $(".vendor").fitVids();
-// Custom selector and No-Double-Wrapping Prevention Test
-$(".vendor").fitVids({ customSelector: "iframe[src^='http://socialcam.com']"});
 
-$(function(){
-  $("a").each(function(){
-    if ($(this).attr("href") == window.location.pathname){
-      $(this).addClass("selected");
-    }
-  });
-});
+$(".vendor").fitVids({ customSelector: "iframe[src^='http://socialcam.com']" });
+
+$(function() { $("a").each(function() { if ($(this).attr("href") == window.location.pathname) { $(this).addClass("selected"); } }); });
